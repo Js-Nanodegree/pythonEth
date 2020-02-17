fff7aa8bcdc8440675fd7a22edfd05e4d5f99701
Slava <webmaster2@exs.cash>
2020-02-17 18:50:08 +0500

    Create local transaction for check transaction in current node

--- main.py
+++ main.py
@@ -111,6 +111,55 @@ class ETH:
             return (500,str(msg))
         return answer
 
+    async def parity_localTransactions(self):
+        '''
+        parity_localTransactionsОн возвращает объект, хэш-ключ для сделки, стоимость сделки объекта
+        :return:
+        (200,{
+              "id": 1,
+              "jsonrpc": "2.0",
+              "result": {
+                "0x09e64eb1ae32bb9ac415ce4ddb3dbad860af72d9377bb5f073c9628ab413c532": {
+                  "status": "mined",
+                  "transaction": {
+                    "from": "0x00a329c0648769a73afac7f9381e08fb43dbea72",
+                    "to": "0x00a289b43e1e4825dbedf2a78ba60a640634dc40",
+                    "value": "0xfffff",
+                    "blockHash": null,
+                    "blockNumber": null,
+                    "creates": null,
+                    "gas": "0xe57e0",
+                    "gasPrice": "0x2d20cff33",
+                    "hash": "0x09e64eb1ae32bb9ac415ce4ddb3dbad860af72d9377bb5f073c9628ab413c532",
+                    "input": "0x",
+                    "condition": {
+                      "block": 1
+                    },
+                    "chainId": null,
+                    "nonce": "0x0",
+                    "publicKey": "0x3fa8c08c65a83f6b4ea3e04e1cc70cbe3cd391499e3e05ab7dedf28aff9afc538200ff93e3f2b2cb5029f03c7ebee820d63a4c5a9541c83acebe293f54cacf0e",
+                    "raw": "0xf868808502d20cff33830e57e09400a289b43e1e4825dbedf2a78ba60a640634dc40830fffff801ca034c333b0b91cd832a3414d628e3fea29a00055cebf5ba59f7038c188404c0cf3a0524fd9b35be170439b5ffe89694ae0cfc553cb49d1d8b643239e353351531532",
+                    "standardV": "0x1",
+                    "v": "0x1c",
+                    "r": "0x34c333b0b91cd832a3414d628e3fea29a00055cebf5ba59f7038c188404c0cf3",
+                    "s": "0x524fd9b35be170439b5ffe89694ae0cfc553cb49d1d8b643239e353351531532",
+                    "transactionIndex": null
+                  }
+                },
+                "0x...": { ... }
+              }
+            })
+        '''
+        data = (1, 'parity_localTransactions', [])
+        params = self.create_params(data)
+        try:
+            answer = await self.request_node(params)
+        except Exception as msg:
+            #   TODO CHECK THIS LOG FILE
+            # raise
+            return (500, str(msg))
+        return answer
+
     async def gas_transaqtion(self):
         # return how much eth need to transaqtion
         return 2.15
