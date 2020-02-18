2668dd9885679182a6d123d525dcb62642bf41d6
Slava <webmaster2@exs.cash>
2020-02-18 16:03:47 +0500

    Example unlock_account and send Transaction

--- Module/ETH.py
+++ Module/ETH.py
@@ -6,10 +6,15 @@ from web3 import Web3
 import abi
 from utils import send_request_aiohttp
 
-wallet_eth='0x7e07c035242eb6874460d8c033eee25a333988d1'
-wallet_usdt='0x3F9579D03d612E07dDc537feC32E8bb0Cc3cB58f'
+wallet_eth = '0x7e07c035242eb6874460d8c033eee25a333988d1'
+wallet_usdt = '0x3F9579D03d612E07dDc537feC32E8bb0Cc3cB58f'
 url = "http://172.17.123.218:8545/"
 smart_contract = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+
+error = {
+    'Web3_Not_Work': {'code': '-200', 'mode': 'Connct to reserved address'},
+
+}
 
 class ETH:
     def __init__(self):
@@ -18,12 +23,14 @@ class ETH:
         self.web3 = Web3(Web3.HTTPProvider(url))
         self.smart_contract = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
         self.smart_contract = None
+        self.key = '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318'
+        self.passphrase = 'JOINMICROSTARK'
 
     # Start Entrypoint
     async def start(self):
-        (mode,address,data) = await self.check_ballance(self.check_sum_address(address = wallet_eth))
+        (mode,address,data) = await self.check_ballance(self._check_sum_address(address = wallet_eth))
         print(mode,address,data)
-        (mode,address,data) = await self.check_ballance(self.check_sum_address(address = wallet_usdt))
+        (mode,address,data) = await self.check_ballance(self._check_sum_address(address = wallet_usdt))
         print(mode,address,data)
         print('start')
         return True
@@ -38,15 +45,10 @@ class ETH:
         (current_contract ,address_wallet, label_wallet)
         '''
         # create address
-        if self.smart_contract is None:
-            print('create account Eth')
-            address = self.web3.parity.personal.newAccount(phrase)
-        else:
-            print('create account Tether')
-            address = 'Tether'
+        address = self.web3.parity.personal.newAccount(phrase)
         # set label for address
         body = [address, label]
-        label_account = await self.parity_setAccountName(body)
+        label_account = await self._parity_setAccountName(body)
         # create task for check_ballance()
         (code,data) = label_account
         if code == 200:
@@ -65,17 +67,22 @@ class ETH:
         :return:
         (address,ballance,smart_contract)
         '''
-        connected = self.web3.isConnected()
-        if connected == True:
+        if self._check_web3_connected() == True:
+            '''
+            connect with Web3 for main Node
+            '''
             if self.smart_contract is None:
                 ballance = self.web3.eth.getBalance(address)
-                return (address,self.from_wei(ballance),None)
+                return (address, self._from_wei(ballance), None)
             else:
-                contract = self.web3.eth.contract(address=self.smart_contract, abi=abi)
+                contract = self._build_contract_usdt()
                 ballance = contract.functions.balanceOf(address).call()
-            return (address,self.from_wei(ballance),self.smart_contract)
+                return (address, self._from_wei(ballance), self.smart_contract)
         else:
-            return (None,None,None)
+            '''
+            Need Connect to Reserved Node
+            '''
+            return (None, None, None)
 
     async def take_list_address(self):
         '''
@@ -87,62 +94,33 @@ class ETH:
         [address_eth_isUpperCase, ..., ...]
         '''
         # return all address in current wallet
-        list = self.web3.parity.personal.listAccounts()
-        # list = await self.parity_allAccountsInfo()
-        return list
+        # list = self.web3.parity.personal.listAccounts()
+        all_list = await self.parity_allAccountsInfo()
+        
+        return all_list
 
-    async def find_address_name(self,address):
+    async def send_transaction(self, body):
         '''
-        find name label in address when user are send
-        TODO NOT WORK WITH ADDRESS IN LOCAL BASE AND CREATE ADDRESS
-
-        :param address: '0xF4D34Bb387a65B459d065238A3f093A89cB54e30'
+        WARN rpc  eth_sendTransaction is deprecated and will be removed in future versions:
+        Account management is being phased out see #9997 for alternatives.
+        :param body:(send_to_wallet,send_from_wallet,value_send)
         :return:
-        (address,name_address)
-        (500,Error message)
+        (code, hash_trx)
         '''
-        data = (1, 'parity_accountsInfo', [])
-        params = self.create_params(data)
+        (send_to_wallet, send_from_wallet, value_send) = body
         try:
-            answer = await self.request_node(params)
+            if self.smart_contract is None:
+                hash = self.web3.eth.sendTransaction(
+                    {'to': send_to_wallet, 'from': send_from_wallet, 'value': value_send})
+            else:
+                # TODO SEARCH METHOD SEND TRANSACTION FOR ERC20
+                hash = None
         except Exception as msg:
-            #   TODO CHECK THIS LOG FILE
-            # raise
-            return (500, str(msg))
-        (code,data) = answer
-        name = data['result']
-        return (address,name[address])
+            return (None,str(msg))
 
-    async def send_transaqtion(self):
-        # if this transaqtion is not blocked
-        # return hash transaqtion
-        return 'dfkjhgnkjdsfnksdjfkjsd%^SA&5d'
+        return (200,hash)
 
-    async def create_transaction(self,body):
-        '''
-        If the transaction specifies a data value but does not specify gas
-        then the gas value will be populated using the estimateGas()
-        function with an additional buffer of 100000 gas up to the gasLimit of the latest block.
-        In the event that the value returned by estimateGas()
-        method is greater than the gasLimit a ValueError will be raised.
-        :param body: (address,value,gas)
-        :return:
-        Returns a transaction that’s been signed by the node’s private key, but not yet submitted.
-        The signed tx can be submitted with Eth.sendRawTransaction`
-        '''
-        (address,value,gas) = body
-        transaction = {
-            'to': address,
-            'value': value,
-            'gas': gas,
-            'gasPrice': self.web3.eth.gasPrice,
-            'nonce': self.web3.eth.getTransactionCount(self.web3.eth.coinbase),
-            'chainId': 1
-        }
-        print(transaction)
-        return transaction
-
-    async def send_raw_transaction(self,transaction):
+    async def _send_raw_transaction(self, transaction):
         '''
         Returns a transaction that’s been signed by the node’s private key, but not yet submitted. The signed tx can be submitted with Eth.sendRawTransaction`
         Sends a signed and serialized transaction. Returns the transaction hash as a HexBytes object.
@@ -159,76 +137,36 @@ class ETH:
         return (200, send_raw_transaction)
 
     # RPC API REQUEST FOR NODE ETHERIUM
-    async def parity_setAccountName(self,body):
+    async def _parity_setAccountName(self, body):
         data = (1, 'parity_setAccountName', body)
-        params = self.create_params(data)
+        params = self._create_params(data)
         try:
-            answer = await self.request_node(params)
-        except Exception as msg:
-            #   TODO CHECK THIS LOG FILE
-            # raise
-            return (500,str(msg))
-        return answer
-
-    async def parity_localTransactions(self):
-        '''
-        parity_localTransactionsОн возвращает объект, хэш-ключ для сделки, стоимость сделки объекта
-        :return:
-        (200,{
-              "id": 1,
-              "jsonrpc": "2.0",
-              "result": {
-                "0x09e64eb1ae32bb9ac415ce4ddb3dbad860af72d9377bb5f073c9628ab413c532": {
-                  "status": "mined",
-                  "transaction": {
-                    "from": "0x00a329c0648769a73afac7f9381e08fb43dbea72",
-                    "to": "0x00a289b43e1e4825dbedf2a78ba60a640634dc40",
-                    "value": "0xfffff",
-                    "blockHash": null,
-                    "blockNumber": null,
-                    "creates": null,
-                    "gas": "0xe57e0",
-                    "gasPrice": "0x2d20cff33",
-                    "hash": "0x09e64eb1ae32bb9ac415ce4ddb3dbad860af72d9377bb5f073c9628ab413c532",
-                    "input": "0x",
-                    "condition": {
-                      "block": 1
-                    },
-                    "chainId": null,
-                    "nonce": "0x0",
-                    "publicKey": "0x3fa8c08c65a83f6b4ea3e04e1cc70cbe3cd391499e3e05ab7dedf28aff9afc538200ff93e3f2b2cb5029f03c7ebee820d63a4c5a9541c83acebe293f54cacf0e",
-                    "raw": "0xf868808502d20cff33830e57e09400a289b43e1e4825dbedf2a78ba60a640634dc40830fffff801ca034c333b0b91cd832a3414d628e3fea29a00055cebf5ba59f7038c188404c0cf3a0524fd9b35be170439b5ffe89694ae0cfc553cb49d1d8b643239e353351531532",
-                    "standardV": "0x1",
-                    "v": "0x1c",
-                    "r": "0x34c333b0b91cd832a3414d628e3fea29a00055cebf5ba59f7038c188404c0cf3",
-                    "s": "0x524fd9b35be170439b5ffe89694ae0cfc553cb49d1d8b643239e353351531532",
-                    "transactionIndex": null
-                  }
-                },
-                "0x...": { ... }
-              }
-            })
-        '''
-        data = (1, 'parity_localTransactions', [])
-        params = self.create_params(data)
-        try:
-            answer = await self.request_node(params)
+            answer = await self._request_node(params)
         except Exception as msg:
             #   TODO CHECK THIS LOG FILE
             # raise
             return (500, str(msg))
         return answer
 
-    async def gas_transaqtion(self):
-        # return how much eth need to transaqtion
-        return 2.15
-
-    async def notify_other(self):
-        # send to matchenger and other resources
-        return 'Ok'
+    async def _unlock_account(self, body):
+        '''
+        Unlocks the given account for duration seconds.
+        If duration is None then the account will remain unlocked indefinitely.
+        Returns boolean as to whether the account was successfully unlocked.
+        :param body: (address,passphrase)
+        :return:
+        (code,Boolean)
+        '''
+        (address, passphrase) = body
+        try:
+            logic = self.web3.parity.personal.unlockAccount(address,passphrase)
+        except Exception as msg:
+            print(str(msg))
+            return (msg['code'],msg['message'],msg['message'])
+        return (200,logic)
 
     # Helper for Request for crypto Nodes Etherium
-    def create_params(self, body):
+    def _create_params(self, body):
         '''
         Create Params for aiohttp request
         :param body: (key_request,method_callable,args_request)
@@ -244,10 +182,84 @@ class ETH:
         params['url'] = self.url
         return params
 
-    async def request_node(self, params):
+    def _create_gas_params(self):
+        '''
+        Create Params for aiohttp request
+        :return
+        [
+            {
+                gasprice: 40,
+                hashpower_accepting: 100,
+                hashpower_accepting2: 97.6744186047,
+                tx_atabove: 3,
+                age: 0,
+                pct_remaining5m: 0,
+                pct_mined_5m: 100,
+                total_seen_5m: 1,
+                average: 400,
+                safelow: 100,
+                nomine: null,
+                avgdiff: 1,
+                intercept: 4.8015,
+                hpa_coef: -0.0243,
+                avgdiff_coef: -1.6459,
+                tx_atabove_coef: 0.0006,
+                int2: 6.9238,
+                hpa_coef2: -0.067,
+                sum: 0.2238,
+                expectedWait: 2,
+                unsafe: 0,
+                expectedTime: 0.52
+            }
+        ]
+        '''
+        (key, method, args) = body
+        params = {
+            'method': 'GET',
+        }
+        params['url'] = 'https://ethgasstation.info/json/ethgasAPI.json'
+        return params
+
+    async def _create_transaction(self, body):
+        '''
+        If the transaction specifies a data value but does not specify gas
+        then the gas value will be populated using the estimateGas()
+        function with an additional buffer of 100000 gas up to the gasLimit of the latest block.
+        In the event that the value returned by estimateGas()
+        method is greater than the gasLimit a ValueError will be raised.
+        :param body: (address,value,gas)
+        :return:
+        Returns a transaction that’s been signed by the node’s private key, but not yet submitted.
+        The signed tx can be submitted with Eth.sendRawTransaction`
+        '''
+        (address,value,gas) = body
+        nonce = _take_nonce(address)
+        gasPrice = self.web3.eth.gasPrice
+
+        if self.smart_contract is None:
+            transaction = {
+                'chainId': 1,
+                'to': address,
+                'value': value,
+                'gas': gas,
+                'gasPrice': gasPrice,
+                'nonce': nonce,
+            }
+        else:
+            contract = self._build_contract_usdt()
+            transaction = contract.functions.transfer(address, value).buildTransaction({
+                    'chainId': 1,
+                    'gas': gas,
+                    'gasPrice': gasPrice,
+                    'nonce': nonce,
+            })
+
+        return transaction
+
+    async def _request_node(self, params):
         '''
         send request for aiohttp and fetch answer data in CryptoNodes ETH
-        :param params: create with method create_params
+        :param params: create with method _create_params
         :return:
         (code,data['result'])
         (200,'Ok')
@@ -262,7 +274,10 @@ class ETH:
             return (code, mode)
 
     # Helper Utils for request Eth Node
-    def check_sum_address(self,address):
+    def _check_web3_connected(self):
+        return self.web3.isConnected()
+
+    def _check_sum_address(self, address):
         '''
         Will convert an upper or lowercase Ethereum address to a checksum address.
 
@@ -276,28 +291,40 @@ class ETH:
         else:
             return None
 
-    def from_wei(self,ballance):
-        if self.smart_contract is None:
-            return self.web3.fromWei(ballance,'ether')
-        else:
-            return self.web3.fromWei(ballance,'mwei')
+    def _take_nonce(self, address):
+        '''
+        Last famous transaction id block for create transaction Id
+        :return:
+        number
+        '''
+        if address is None:
+            address = self.web3.eth.coinbase
+        nonce = self.web3.eth.getTransactionCount(address)
+        return nonce
 
-    def hex_to_string(self,hex):
+    def _build_contract_usdt(self):
+        return self.web3.eth.contract(address=self.smart_contract, abi=abi)
+
+    def _from_wei(self, ballance):
+        if self.smart_contract is None:
+            return self.web3.fromWei(ballance, 'ether')
+        else:
+            return self.web3.fromWei(ballance, 'mwei')
+
+    def _hex_to_string(self, hex):
         '''
         Returns the number representation of a given HEX value as a string.
         :param hex: 0xea
         :return:
         "234"
         '''
-        answer = self.web3.hexToNumberString(hex)
-        return answer
+        return self.web3.hexToNumberString(hex)
 
-    def number_to_hex(self,number):
+    def _number_to_hex(self, number):
         '''
         Returns the HEX representation of a given number value.
         :param number: "234"
         :return:
         0xea
         '''
-        answer = self.web3.numberToHex(number)
-        return answer
+        return self.web3.numberToHex(number)
--- example.py
+++ example.py
@@ -37,17 +37,26 @@ wallet_usdt='0x3F9579D03d612E07dDc537feC32E8bb0Cc3cB58f'
 url = "http://172.17.123.218:8545/"
 smart_contract = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
 
+abi=json.loads('[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]')
+
+
 class Example:
     def __init__(self):
         self.loop = asyncio.get_event_loop()
         self.url = url
         self.web3 = Web3(Web3.HTTPProvider(url))
         self.smart_contract = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-        self.smart_contract = None
+        # self.smart_contract = None
         self.key = '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318'
 
     async def start(self):
-        body = ('0xd3CdA913deB6f67967B99D67aCDFa1712C293601', 500, 3)
+        '''
+        ValueError: {'code': -32010, 'message': 'Transaction gas is too low.
+        There is not enough gas to cover minimal cost of the transaction (minimal: 21584, got: 3).
+        Try increasing supplied gas.'}
+        :return:
+        '''
+        body = (wallet_usdt, 500, 21)
         transaction = await self.create_transaction(body)
         data = await self.send_raw_transaction(transaction)
         print(data)
@@ -60,28 +69,33 @@ class Example:
         function with an additional buffer of 100000 gas up to the gasLimit of the latest block.
         In the event that the value returned by estimateGas()
         method is greater than the gasLimit a ValueError will be raised.
-        :param body: (address,value,gas)
+        :param body:
         :return:
-        Returns a transaction that’s been signed by the node’s private key, but not yet submitted.
-        The signed tx can be submitted with Eth.sendRawTransaction`
         '''
         (address,value,gas) = body
-        transaction = {
-            'to': address,
-            'value': value,
-            'gas': gas,
-            'gasPrice': self.web3.eth.gasPrice,
-            'nonce': self.web3.eth.getTransactionCount(self.web3.eth.coinbase),
-            'chainId': 1
-        }
-        print(transaction)
+        contract = self.web3.eth.contract(address=self.smart_contract, abi=abi)
+        print(self.web3.eth.getTransactionCount(self.web3.eth.coinbase))
+        print(self.web3.toWei(12, "gwei"))
+        transaction = contract.functions.transfer(address, value).buildTransaction(
+            {
+                'chainId': 1,
+                'gas': gas,
+                'gasPrice':self.web3.toWei('1', "gwei"),
+                'nonce': self.web3.eth.getTransactionCount(self.web3.eth.coinbase),
+            }
+        )
         return transaction
+
+    def to_hex(self,data):
+        return self.web3.toHex(data)
 
     async def send_raw_transaction(self,transaction):
         '''
         Returns a transaction that’s been signed by the node’s private key, but not yet submitted. The signed tx can be submitted with Eth.sendRawTransaction`
+
         Sends a signed and serialized transaction. Returns the transaction hash as a HexBytes object.
         :param transaction: form create raw transaction
+
         :return:
         HexBytes('0xe670ec64341771606e55d6b4ca35a1a6b75ee3d5145a99d05921026d1527331')
         '''
