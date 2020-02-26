131ed85b84c9483a80b5cc35e4e1eab47b77a344
Slava <webmaster2@exs.cash>
2020-02-26 16:01:53 +0500

    new commit

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name='aiow3parity',
-      version='1.1',
-      description='Easy go to parity w3 transaction',
-      url='https://github.com/Js-Nanodegree/pythonEth',
-      author='Js-Nanodegree',
-      author_email='js-nanodegree@gmail.com',
-      license='MIT',
-      packages=['aiow3parity'],
-      install_requires=[],
-)
+# setup(name='aiow3parity',
+#       version='1.1',
+#       description='Easy go to parity w3 transaction',
+#       url='https://github.com/Js-Nanodegree/pythonEth',
+#       author='Js-Nanodegree',
+#       author_email='js-nanodegree@gmail.com',
+#       license='MIT',
+#       packages=['aiow3parity'],
+#       install_requires=[],
+# )
 setup(
     name='aiow3parity',
     version='0.6.4',
