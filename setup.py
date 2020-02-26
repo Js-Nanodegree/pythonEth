4d4f1dc20edb8c4f12843ecd67b3bd38140a3480
Slava <webmaster2@exs.cash>
2020-02-26 15:57:33 +0500

    new commit

--- setup.py
+++ setup.py
@@ -8,5 +8,29 @@ setup(name='aiow3parity',
       author_email='js-nanodegree@gmail.com',
       license='MIT',
       packages=['aiow3parity'],
-      install_requires=['web3', "asyncio", "json", "aiohttp", "async_timeout"],
+      install_requires=[],
+)
+setup(
+    name='aiow3parity',
+    version='0.6.4',
+    description='acquire data published on the smartfact web page',
+    url='https://github.com/Js-Nanodegree/pythonEth',
+    author='Dominik Neise, Sebastian Mueller, Maximilian Nöthe',
+    author_email='jsnanodegree@gmail.com',
+    license='MIT',
+    packages=[
+        'aiow3parity',
+    ],
+    package_data={
+        'aiow3parity': [
+            'resources/20160703_233149/*.data',
+            'resources/20160703_233149_broken_fsc/fsc.data',
+        ]
+    },
+    install_requires=[
+        'web3', "asyncio", "json", "aiohttp", "async_timeout"
+    ],
+#     tests_require=['pytest>=3.0', 'freezegun'],
+#     setup_requires=['pytest-runner'],
+    zip_safe=True,
 )
